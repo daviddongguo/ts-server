14348096a6f02e4d5bfb0ca7abcd71abeb7c6aa7
daviddongguo <david.dong.guo@gmail.com>
2021-03-04 19:38:53 -0500

    Add bodyParser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
+import bodyParser from 'body-parser';
 import express, {Request, Response} from 'express';
 import router from './routes/loginRoutes';
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req: Request, res: Response) => {
 	return res.status(200).send('success');
