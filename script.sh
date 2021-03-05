81f664c40122441c8eb129be396723ec09fb31bc
daviddongguo <david.dong.guo@gmail.com>
2021-03-04 20:12:52 -0500

    add remote repository

--- script.sh
+++ script.sh
@@ -3,3 +3,8 @@ tsc --init
 npm install concurrently nodemon
 npm install express body-parser cookie-session
 npm install @types/express @types/body-parser @types/cookie-session
+
+
+git remote add origin https://github.com/daviddongguo/ts-server.git
+git branch -M main
+git push -u origin main
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import express, {Request, Response} from 'express';
 import router from './routes/loginRoutes';
 
 const app = express();
-const port = 3000;
+const port = 3011;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({keys: ['']}));
