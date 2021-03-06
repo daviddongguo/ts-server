5df615b3ed3fcff6659768f255a04d1dc58fce3e
daviddongguo <david.dong.guo@gmail.com>
2021-03-05 22:09:47 -0500

    Add AppRoter to get single instance

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import express, {Request, Response} from 'express';
-import router from './routes/authRoutes';
-
+import express from 'express';
+import {AppRouter} from './AppRouter';
+// import router from './routes/authRoutes';
+import './controllers/AuthController';
 const app = express();
 const port = 3011;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({keys: ['']}));
 
-router.get('/status', (req: Request, res: Response) => {
-	return res.status(200).send('Live ....');
-});
-app.use(router);
+// router.get('/status', (req: Request, res: Response) => {
+// 	return res.status(200).send('Live ....');
+// });
+app.use(AppRouter.getInstance());
 
 app.listen(port, () => {
 	console.log(`Listening on port ${port}`);
